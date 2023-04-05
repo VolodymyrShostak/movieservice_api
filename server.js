55647e94c2b075c8c6369148ae73069fb344cd48
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2023-04-05 11:23:37 +0300

    some changes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const app = require("./src/app");
 const { connectMongoDB } = require("./src/db/connection");
+require("dotenv").config();
 
 const start = async () => {
   try {
     await connectMongoDB();
     console.log("Database connection successful");
 
-    app.listen(3002, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server running. Use our API on port: 3002");
     });
   } catch (error) {
